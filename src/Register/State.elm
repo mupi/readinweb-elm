3bae367a19e32d1d72dbdf0b7b845ae4a374af26
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2016-09-26 12:00:57 -0300

    Accidentally removed some code

--- src/Register/State.elm
+++ src/Register/State.elm
@@ -62,3 +62,53 @@ update msg model =
 
         RegisterError error ->
             ( { model | error = toString error }, Cmd.none )
+
+
+type ErrorType
+    = ArrayError (Array String)
+    | StringError String
+
+
+errorDecoder : Decoder ErrorType
+errorDecoder =
+    oneOf
+        [ Decode.map ArrayError ("username" := Decode.array Decode.string)
+        , Decode.map StringError ("error" := Decode.string)
+        ]
+
+
+
+-- API register
+
+
+registerUrl : String
+registerUrl =
+    "http://localhost:8000/riw/users/register/"
+
+
+userEncoder : Model -> Encode.Value
+userEncoder model =
+    Encode.object
+        [ ( "username", Encode.string model.user.username )
+        , ( "name", Encode.string model.user.name )
+        , ( "password", Encode.string model.user.password )
+        , ( "email", Encode.string model.user.email )
+        ]
+
+
+tokenDecoder : Decoder String
+tokenDecoder =
+    "token" := Decode.string
+
+
+registerUser : Model -> String -> Task (HttpBuilder.Error String) (HttpBuilder.Response String)
+registerUser model registerUrl =
+    HttpBuilder.post registerUrl
+        |> HttpBuilder.withHeader "Content-Type" "application/json"
+        |> HttpBuilder.withJsonBody (userEncoder model)
+        |> HttpBuilder.send (HttpBuilder.jsonReader tokenDecoder) HttpBuilder.stringReader
+
+
+registerUserCmd : Model -> String -> Cmd Msg
+registerUserCmd model apiUrl =
+    Task.perform RegisterError RegisterSuccess <| registerUser model apiUrl
